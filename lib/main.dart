b59a5ae0201c751d275b4964e6ceacef491e5c3f
Leonardo Mendes <leonardo_mendes2002@hotmail.com>
2026-01-12 19:58:55 -0300

    Iniciando Projeto

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main () => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Hello, World!"),
+    );
+  }
+}
